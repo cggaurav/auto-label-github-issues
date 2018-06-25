b44e7f3a9dd3b5823d0cffcd11f63d3adc980ed6
Gaurav Chandrashekar <cggaurav@gmail.com>
2018-06-24 21:06:50 -0500

    Some updates

--- data/export.js
+++ data/export.js
@@ -40,36 +40,69 @@ const REPOSITORES = [
 // TODO: Rate limiting
 // TODO: Pagination
 
+function writeIssueToFile(issue, labels) {
+	// FORMAT: https://api.github.com/repos/kubernetes/kubernetes/issues?state=closed&page=1&per_page=1
+	console.log(`Writing issue ${issue.url} with labels ${JSON.stringify(labels)}`)
+
+	fs.appendFileSync(FILENAME, [[issue.url, issue.id, `${issue.title.replace(/,/g, ' ').replace(/\r|\n/g, ' ')}`, `${issue.body.replace(/,/g, ' ').replace(/\r|\n/g, ' ')}`, labels.join('|')].join(','), '\n'].join(''))
+}
+
+
+function makeGithubIssueRequest(repository, page = 1, per_page = 10) {
+
+	return new Promise((resolve, reject) => {
+		let ENDPOINT = `https://api.github.com/repos/${repository}/issues?state=closed&page=${page}&per_page=${per_page}`
+
+		console.log(`Making a request for ${repository} and page ${page}`)
+
+		console.log(process.env.GITHUB_TOKEN)
+
+		return axios.get(ENDPOINT, {
+			headers: {
+				'Authorization': `token ${process.env.GITHUB_TOKEN}`
+			}
+		})
+		.then((body) => {
+			// FORMAT: https://api.github.com/repos/kubernetes/kubernetes/issues?state=closed&page=1&per_page=1
+			const issues = body.data
+
+			if (issues.length === 0) {
+				return resolve()
+			}
+
+			issues.forEach((issue, index) => {
+
+				let _labels = []
+
+				issue.labels.forEach((issue_label) => {
+					LABELS.forEach((label) => {
+						if (issue_label.name.toLowerCase().includes(label)) {
+							_labels.push(label)
+						}
+					})
+				})
+				
+				if (_labels.length > 0) {
+					writeIssueToFile(issue, _labels)
+				}
+			})
+
+			setTimeout(() => {
+				return makeGithubIssueRequest(repository, page + 1)
+			}, 300)
+
+		})
+		.catch((err) => {
+			// console.error(err)
+			return reject(err)
+		})	
+	})
+}
+
 Promise.each(REPOSITORES, (repository) => {
 	return new Promise((resolve, reject) => {
-		let ENDPOINT = `https://api.github.com/repos/${repository}/issues?state=closed&page=1&per_page=10`
-		return axios.get(ENDPOINT)
-		  	.then((body) => {
-		  		// FORMAT: https://api.github.com/repos/kubernetes/kubernetes/issues?state=closed&page=1&per_page=1
-		  		const issues = body.data
 
-		  		issues.forEach((issue) => {
-
-		  			let _labels = []
-
-		  			issue.labels.forEach((issue_label) => {
-		  				LABELS.forEach((label) => {
-		  					if (issue_label.name.toLowerCase().includes(label)) {
-		  						_labels.push(label)
-		  					}
-		  				})
-		  			})
-		  			
-		  			if (_labels.length > 0) {
-		  				fs.appendFileSync(FILENAME, [[issue.url, issue.id, `${issue.body.replace(/,g/, '')}`, _labels].join(','), '\n'].join(''))
-		  				return resolve(_labels)
-		  			} else {
-		  				return resolve(null)
-		  			}
-		  		})
-		  	})
-		  	.catch((err) => {
-		  		console.error(err)
-		  	})
+		return makeGithubIssueRequest(repository)
+		
 	})
 })

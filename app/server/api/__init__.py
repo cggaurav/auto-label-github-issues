55fab62b2a50cea579c05b9aea7a8a9c89299074
Gaurav C <cggaurav@gmail.com>
2018-09-02 18:30:15 -0500

    Seems like that is how gunicorn works

--- app/server/api/__init__.py
+++ app/server/api/__init__.py
@@ -1,14 +1,14 @@
 import os
 from flask import Flask
 from flask import request
-app = Flask(__name__)
+api = Flask(__name__)
 
 # TODO: Connect this
-@app.route('/')
+@api.route('/')
 def hello_world():
     return 'Hello, Auto Label Github issues!'
 
-@app.route('/predict')
+@api.route('/predict')
 def predict():
 	# TODO: Connect to the model
 	# /predict?issue=x
@@ -16,4 +16,4 @@ def predict():
 	return issue
 
 if __name__ == "__main__":
-    app.run()
+    api.run()
